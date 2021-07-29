f092d548329b25a631e631bf1d5169baaef37b72
dnyana <dnyaneshgawade903@gmail.com>
2021-07-29 08:47:38 +0530

    [Dnyanesh] add : NLOG tutorial

---
+++ Addition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NlogProb
+{
+    class Addition
+    {
+        static NLog nlog = new NLog();
+        public int Add(int num1, int num2)
+        {
+            if (num1 == 0 || num2 == 0)
+            {
+                nlog.LogInfo("Sub method");
+                nlog.LogDebug("Debug Successful");
+                nlog.LogError("First Number or Second Number is zero.");
+                return 0;
+            }
+            int sum = num1+num2;
+            nlog.LogInfo("Sub method");
+            nlog.LogDebug("Debug Successful");
+            nlog.LogWarn("First Number or Second Number is zero.");
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NlogProb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to NLog");
+            Addition addition = new Addition();
+            addition.Add(10, 20);
+        }
+    }
+}
